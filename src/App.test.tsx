51174be62a3cd6982d6df9eada1b31b77a3bd4cb
jungjinbeom <insunok0715@naver.com>
2024-01-12 00:34:33 +0900

    Keep calm and commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,12 +5,12 @@ import '@testing-library/jest-dom';
 import IntroPage from './pages/IntroPage';
 
 test('event route', async () => {
-  // const routes = [
-  //   {
-  //     path: '/intro',
-  //     element: <IntroPage />,
-  //   },
-  // ];
+  const routes = [
+    {
+      path: '/intro',
+      element: <IntroPage />,
+    },
+  ];
   // const router = createMemoryRouter(routes, {
   //   initialEntries: ['/', '/events/123'],
   //   initialIndex: 1,
