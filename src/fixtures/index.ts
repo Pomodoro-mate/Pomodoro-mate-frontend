c72b30ad337b5ac2e2c833984a3dc0990d8775bf
kkkkkse <eun2abc@gmail.com>
2024-03-15 03:00:46 +0900

    Chore: 스터디룸 목록 관련 테스트를 위한 fixture 데이터 추가

---
+++ src/fixtures/index.ts
@@ -0,0 +1,9 @@
+import studyRoom from '@/fixtures/study-room';
+import studyRooms from '@/fixtures/study-rooms';
+import pageDto from '@/fixtures/page-dto';
+
+export default {
+  studyRoom,
+  studyRooms,
+  pageDto,
+};
---
+++ src/fixtures/study-rooms.ts
@@ -0,0 +1,69 @@
+import { StudyRoom } from '@/types/study-rooms.types';
+
+const studyRooms: StudyRoom[] = [
+  {
+    id: 1,
+    name: '스터디룸1',
+    intro: '스터디룸 설명1',
+    step: 'PLANNING',
+    participantCount: 1,
+  },
+  {
+    id: 2,
+    name: '스터디룸2',
+    intro: '스터디룸 설명2',
+    step: 'PLANNING',
+    participantCount: 2,
+  },
+  {
+    id: 3,
+    name: '스터디룸3',
+    intro: '스터디룸 설명3',
+    step: 'PLANNING',
+    participantCount: 3,
+  },
+  {
+    id: 4,
+    name: '스터디룸4',
+    intro: '스터디룸 설명4',
+    step: 'PLANNING',
+    participantCount: 4,
+  },
+  {
+    id: 5,
+    name: '스터디룸5',
+    intro: '스터디룸 설명5',
+    step: 'PLANNING',
+    participantCount: 5,
+  },
+  {
+    id: 6,
+    name: '스터디룸6',
+    intro: '스터디룸 설명6',
+    step: 'PLANNING',
+    participantCount: 6,
+  },
+  {
+    id: 7,
+    name: '스터디룸7',
+    intro: '스터디룸 설명7',
+    step: 'PLANNING',
+    participantCount: 7,
+  },
+  {
+    id: 8,
+    name: '스터디룸8',
+    intro: '스터디룸 설명8',
+    step: 'PLANNING',
+    participantCount: 8,
+  },
+  {
+    id: 9,
+    name: '스터디룸9',
+    intro: '스터디룸 설명9',
+    step: 'PLANNING',
+    participantCount: 6,
+  },
+];
+
+export default studyRooms;
