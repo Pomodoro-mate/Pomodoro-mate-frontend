59fa00e0d96c863541db2ed735a1d14363d7292e
kkkkkse <eun2abc@gmail.com>
2024-03-17 17:01:07 +0900

    Refactor: 데이터 타입 지정 시점을 쿼리 -> api 호출 시점으로 변경

--- src/apis/study-rooms/get-study-rooms.ts
+++ src/apis/study-rooms/get-study-rooms.ts
@@ -1,6 +1,12 @@
 import { http } from '../api';
+import { PageDto, StudyRoom } from '@/types/study-rooms.types';
 
-export const getStudyRooms = async ({ page }: { page: number }) => {
+interface DataType {
+  studyRooms: StudyRoom[];
+  pageDto: PageDto;
+}
+
+export const getStudyRooms = async ({ page }: { page: number }): Promise<DataType> => {
   const { data } = await http.get('/studyrooms', { params: { page } });
 
   return data;
