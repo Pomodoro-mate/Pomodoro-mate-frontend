62796212408d7e7d73d764677e0eeb51cdb723c6
kkkkkse <eun2abc@gmail.com>
2024-03-17 17:01:07 +0900

    Refactor: 데이터 타입 지정 시점을 쿼리 -> api 호출 시점으로 변경

--- src/hooks/useStudyRoomsQuery.ts
+++ src/hooks/useStudyRoomsQuery.ts
@@ -1,17 +1,11 @@
 import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { StudyRoom, PageDto } from '@/types/study-rooms.types';
 import { getStudyRooms } from '@/apis/study-rooms/get-study-rooms';
-
-interface QueryData {
-  studyRooms: StudyRoom[];
-  pageDto: PageDto;
-}
 
 const useStudyRoomsQuery = () => {
   const [page, setPage] = useState(1);
 
-  const { isLoading, isError, data } = useQuery<QueryData>({
+  const { isLoading, isError, data } = useQuery({
     queryKey: ['study-room-list', page],
     queryFn: () => getStudyRooms({ page }),
   });
