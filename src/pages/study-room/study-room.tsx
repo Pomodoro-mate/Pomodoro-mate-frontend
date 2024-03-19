efc0795daa622afe583a30021855d8fb33754c60
jungjinbeom <insunok0715@naver.com>
2024-03-20 00:40:14 +0900

    feat : 스터디룸 레이아웃 설정 및 컴포넌트 추가 - 레이아웃 컴포넌트 생성 - MUI 라이브러리 컴포넌트 활용하여 레이아웃 설정

--- src/pages/study-room/study-room.tsx
+++ src/pages/study-room/study-room.tsx
@@ -1,51 +1,34 @@
-import { Button, Card, CardContent, CardHeader, Chip } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Chip, Grid } from '@mui/material';
+import Layout from './components/layout';
 import ParticipantList from './components/participant-list';
 import Timer from './components/timer';
 
 const StudyRoom = () => {
   return (
-    <div
-      style={{
-        width: '100%',
-        display: 'flex',
-        gap: '4px',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <div style={{ display: 'flex', height: '300px', marginBottom: '0.5rem', gap: '10px' }}>
-        <div style={{ width: '65%' }}>
-          <Timer />
-        </div>
-        <div style={{ width: '35%' }}>
-          <ParticipantList />
-        </div>
-      </div>
-      <div style={{ display: 'flex', height: '300px', gap: '10px' }}>
-        <div style={{ width: '65%' }}>
-          <Card sx={{ minWidth: '50%' }}>
-            <CardHeader title="학습 키워드" />
-            <CardContent sx={{ height: '130px', textAlign: 'center' }}>
-              <input type="text" />
-              <Button size="small">등록</Button>
-              <Chip label="Chip Outlined" variant="outlined" />
-            </CardContent>
-            {/* <CardActions>
-              <Button size="small">시작</Button>
-            </CardActions> */}
-          </Card>
-        </div>
-        <div style={{ width: '35%' }}>
-          <Card sx={{ minWidth: '50%' }}>
-            <CardHeader title="채팅창" />
-            <CardContent sx={{ height: '130px', textAlign: 'center' }}></CardContent>
-            {/* <CardActions>
-              <Button size="small">시작</Button>
-            </CardActions> */}
-          </Card>
-        </div>
-      </div>
-    </div>
+    <Layout>
+      <Grid item xs={6}>
+        <Timer />
+      </Grid>
+      <Grid item xs={6}>
+        <ParticipantList />
+      </Grid>
+      <Grid item xs={6}>
+        <Card sx={{ minWidth: '50%' }}>
+          <CardHeader title="학습 키워드" />
+          <CardContent sx={{ height: '130px', textAlign: 'center' }}>
+            <input type="text" />
+            <Button size="small">등록</Button>
+            <Chip label="Chip Outlined" variant="outlined" />
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <Card sx={{ minWidth: '50%' }}>
+          <CardHeader title="채팅창" />
+          <CardContent sx={{ height: '130px', textAlign: 'center' }}></CardContent>
+        </Card>
+      </Grid>
+    </Layout>
   );
 };
 
