996665f718464d8a01d895e415a7fb3b53afe9d4
kkkkkse <eun2abc@gmail.com>
2024-03-24 16:51:02 +0900

    Refactor: api 호출 시 받아오는 데이터 타입 지정

--- src/apis/study-room/create-study-room.ts
+++ src/apis/study-room/create-study-room.ts
@@ -1,6 +1,16 @@
 import { http } from '../api';
 
-export const createStudyRoom = async ({ name, intro }: { name: string; intro: string }) => {
+interface DataType {
+  id: number;
+}
+
+export const createStudyRoom = async ({
+  name,
+  intro,
+}: {
+  name: string;
+  intro: string;
+}): Promise<DataType> => {
   const { data } = await http.post('/studyrooms', { name, intro });
 
   return data;
