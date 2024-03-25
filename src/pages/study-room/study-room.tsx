6fa28d37362624d64ff6e17e2577d093f669061f
jungjinbeom <insunok0715@naver.com>
2024-03-25 22:52:33 +0900

    chore: 타이머 Conflict 수정 - timer 컴포넌트 Props Conflict 수정

--- src/pages/study-room/study-room.tsx
+++ src/pages/study-room/study-room.tsx
@@ -1,5 +1,4 @@
-import { Button, Card, CardContent, CardHeader, Chip, Grid } from '@mui/material';
-import Layout from './components/layout';
+import { Box, Button, Card, CardContent, CardHeader, Chip, Container, Grid } from '@mui/material';
 import ParticipantList from './components/participant-list';
 import Timer from './components/timer';
 import { useParams } from 'react-router-dom';
@@ -14,30 +13,34 @@ const StudyRoom = () => {
     return <></>;
   }
   return (
-    <Layout>
-      <Grid item xs={6}>
-        <Timer />
-      </Grid>
-      <Grid item xs={6}>
-        <ParticipantList />
-      </Grid>
-      <Grid item xs={6}>
-        <Card sx={{ minWidth: '50%' }}>
-          <CardHeader title="학습 키워드" />
-          <CardContent sx={{ height: '130px', textAlign: 'center' }}>
-            <input type="text" />
-            <Button size="small">등록</Button>
-            <Chip label="Chip Outlined" variant="outlined" />
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={6}>
-        <Card sx={{ minWidth: '50%' }}>
-          <CardHeader title="채팅창" />
-          <CardContent sx={{ height: '130px', textAlign: 'center' }}></CardContent>
-        </Card>
-      </Grid>
-    </Layout>
+    <Container maxWidth="lg">
+      <Box component="section" sx={{ p: 2 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Timer data={data} refetch={refetch} />
+          </Grid>
+          <Grid item xs={6}>
+            <ParticipantList />
+          </Grid>
+          <Grid item xs={6}>
+            <Card sx={{ minWidth: '50%' }}>
+              <CardHeader title="학습 키워드" />
+              <CardContent sx={{ height: '130px', textAlign: 'center' }}>
+                <input type="text" />
+                <Button size="small">등록</Button>
+                <Chip label="Chip Outlined" variant="outlined" />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card sx={{ minWidth: '50%' }}>
+              <CardHeader title="채팅창" />
+              <CardContent sx={{ height: '130px', textAlign: 'center' }}></CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
