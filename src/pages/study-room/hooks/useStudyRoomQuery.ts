5b1533a4b7dd0dd8ad959745631ea6c0732f7636
kkkkkse <eun2abc@gmail.com>
2024-03-31 17:37:12 +0900

    Feat: 스터디룸 정보 쿼리 init 데이터 설정

--- src/pages/study-room/hooks/useStudyRoomQuery.ts
+++ src/pages/study-room/hooks/useStudyRoomQuery.ts
@@ -1,13 +1,25 @@
-import { getStudyRoomInfo } from '@/apis/study-room/get-study-room-info';
 import { useQuery } from '@tanstack/react-query';
+import { StudyRoomInfo } from '@/types/study-room.types';
+import { getStudyRoomInfo } from '@/apis/study-room/get-study-room-info';
 
 interface UseStudyRoomQuery {
   studyId: number;
 }
+
+const initStudyRoom: StudyRoomInfo = {
+  id: 0,
+  name: '',
+  intro: '',
+  step: 'PLANNING',
+  participantSummaries: [],
+  updateAt: '',
+};
+
 const useStudyRoomQuery = ({ studyId }: UseStudyRoomQuery) => {
   return useQuery({
     queryKey: ['get-study-room-info'],
     queryFn: () => getStudyRoomInfo({ studyId }),
+    initialData: initStudyRoom,
   });
 };
 
