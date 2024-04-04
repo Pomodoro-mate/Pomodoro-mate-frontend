f9475fb763e7e654ec86bbe41932a390a01b61a9
kkkkkse <eun2abc@gmail.com>
2024-04-05 02:11:45 +0900

    Feat: 스터디룸 조회 구현 및 기존 레이아웃 변경

--- src/pages/study-room/components/participant-list-item.tsx
+++ src/pages/study-room/components/participant-list-item.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { ParticipantSummary } from '@/types/study-room.types';
 
-const ParticipantListItem = () => {
-  return <div></div>;
+interface ParticipantListItemProps {
+  participant: ParticipantSummary;
+}
+
+const ParticipantListItem = ({ participant }: ParticipantListItemProps) => {
+  const { nickname, imageUrl } = participant;
+
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar alt={nickname} src={imageUrl} />
+      </ListItemAvatar>
+      <ListItemText>{nickname}</ListItemText>
+    </ListItem>
+  );
 };
 
 export default ParticipantListItem;
