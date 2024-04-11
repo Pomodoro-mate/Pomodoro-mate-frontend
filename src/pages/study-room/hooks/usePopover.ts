ed47d01b8ff46f3afad3d2068a9e22f57184c4b2
kkkkkse <eun2abc@gmail.com>
2024-04-11 13:38:11 +0900

    Docs: usePopover의 anchorEl 네이밍에 대한 주석 추가

--- src/pages/study-room/hooks/usePopover.ts
+++ src/pages/study-room/hooks/usePopover.ts
@@ -7,6 +7,7 @@ export interface UsePopover {
 }
 
 const usePopover = () => {
+  // MUI의 Popover Component 속성명에 따른 네이밍 (popover을 트리거하는 button)
   const [anchorEl, setAnchorEl] = useState<UsePopover['anchorEl']>(null);
 
   const openPopover = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
