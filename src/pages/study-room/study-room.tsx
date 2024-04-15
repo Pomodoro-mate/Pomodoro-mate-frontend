4540258205753d88ef623cffc053031f42997ded
kkkkkse <eun2abc@gmail.com>
2024-04-15 15:51:53 +0900

    Feat: 참여자 목록 실시간 업데이트 구현(소켓)

--- src/pages/study-room/study-room.tsx
+++ src/pages/study-room/study-room.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Container, Grid } from '@mui/material';
+import { ParticipantSummary } from '@/types/study-room.types';
 import useStudyRoomQuery from './hooks/useStudyRoomQuery';
+import useSockJSContext from './hooks/useSockJSContext';
 import Spinner from '@/components/common/spinner/spinner';
 import Header from './components/header';
 import Timer from './components/timer';
 import ParticipantPopover from './components/participant-list';
 
 const StudyRoom = () => {
+  const [participants, setParticipants] = useState<ParticipantSummary[]>([]);
+
   const { id: studyId } = useParams();
 
   // 추후 수정 예정
+
   const { isLoading, data, isError } = useStudyRoomQuery({ studyId: Number(studyId) });
 
   const { name, participantSummaries } = data;
+
+  const { curParticipants } = useSockJSContext();
+
+  useEffect(() => {
+    if (curParticipants.length > 0) {
+      setParticipants(curParticipants);
+    } else {
+      setParticipants(participantSummaries);
+    }
+  }, [participantSummaries, curParticipants]);
 
   if (isLoading) {
     return <Spinner />;
@@ -27,7 +43,7 @@ const StudyRoom = () => {
       <Header name={name} />
       <Container maxWidth="xl">
         <Box component="section" sx={{ position: 'relative', paddingBlock: 2 }}>
-          <ParticipantPopover participants={participantSummaries} />
+          <ParticipantPopover participants={participants} />
           <Grid container sx={{ justifyContent: 'center' }}>
             <Grid item xs={6}>
               <Timer data={data} />
