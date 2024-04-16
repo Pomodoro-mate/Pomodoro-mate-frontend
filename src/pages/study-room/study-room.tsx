ecb8a950755afce9445c2987c0111bd0db828c57
kkkkkse <eun2abc@gmail.com>
2024-04-16 16:54:37 +0900

    Refactor: 참가자 목록 데이터 실시간 갱식 방식 변경

--- src/pages/study-room/study-room.tsx
+++ src/pages/study-room/study-room.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Container, Grid } from '@mui/material';
-import { ParticipantSummary } from '@/types/study-room.types';
 import useStudyRoomQuery from './hooks/useStudyRoomQuery';
 import useSockJSContext from './hooks/useSockJSContext';
 import Spinner from '@/components/common/spinner/spinner';
@@ -10,25 +8,16 @@ import Timer from './components/timer';
 import ParticipantPopover from './components/participant-list';
 
 const StudyRoom = () => {
-  const [participants, setParticipants] = useState<ParticipantSummary[]>([]);
-
   const { id: studyId } = useParams();
 
   // 추후 수정 예정
-
   const { isLoading, data, isError } = useStudyRoomQuery({ studyId: Number(studyId) });
 
   const { name, participantSummaries } = data;
 
   const { curParticipants } = useSockJSContext();
 
-  useEffect(() => {
-    if (curParticipants.length > 0) {
-      setParticipants(curParticipants);
-    } else {
-      setParticipants(participantSummaries);
-    }
-  }, [participantSummaries, curParticipants]);
+  const participants = curParticipants.length > 0 ? curParticipants : participantSummaries;
 
   if (isLoading) {
     return <Spinner />;
