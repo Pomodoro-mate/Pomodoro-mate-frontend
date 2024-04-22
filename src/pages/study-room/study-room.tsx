3cad6491cbc7f2284b162eb19d0256248a04b85e
jungjinbeom <insunok0715@naver.com>
2024-04-22 23:05:29 +0900

    feat: 뒤로가기 버튼 조작 - 뒤로가기 버튼 클릭 시 방 나가기 여부 모달 창 기능 구현

--- src/pages/study-room/study-room.tsx
+++ src/pages/study-room/study-room.tsx
@@ -1,10 +1,19 @@
-import { useParams } from 'react-router-dom';
-import { Box, Container, Grid } from '@mui/material';
-import useStudyRoomQuery from './hooks/useStudyRoomQuery';
 import Spinner from '@/components/common/spinner/spinner';
 import Header from './components/header';
-import Timer from './components/timer';
 import ParticipantPopover from './components/participant-list';
+import Timer from './components/timer';
+
+import Modal from '@/components/common/modal/modal';
+import { Box, Container, Grid, Typography } from '@mui/material';
+
+import { useEffect, useState } from 'react';
+import { useBlocker, useNavigate, useParams } from 'react-router-dom';
+
+import useStudyRoomQuery from './hooks/useStudyRoomQuery';
+
+import { ROUTE_PATH } from '@/constant/routes';
+import { getLocalStorage } from '@/utils/storage';
+import useExitStudyRoom from './hooks/useExitStudyRoom';
 
 const StudyRoom = () => {
   const { id: studyId } = useParams();
@@ -13,6 +22,39 @@ const StudyRoom = () => {
   const { isLoading, data, isError } = useStudyRoomQuery({ studyId: Number(studyId) });
 
   const { name, participantSummaries } = data;
+
+  const blocker = useBlocker(
+    ({ currentLocation, nextLocation }) => currentLocation.pathname !== nextLocation.pathname,
+  );
+
+  // 모달
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const navigate = useNavigate();
+  const { mutate } = useExitStudyRoom();
+
+  const exitStudyRoom = () => {
+    const participantId = getLocalStorage('participantId');
+    mutate({ studyRoomId: Number(studyId), participantId: Number(participantId) });
+
+    if (blocker?.proceed) blocker.proceed();
+
+    toggle();
+    navigate(ROUTE_PATH.STUDY_ROOMS);
+  };
+
+  useEffect(() => {
+    const handlePopState = () => toggle();
+    window.addEventListener('popstate', handlePopState);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    // return () => {
+    //   window.addEventListener('popstate', handlePopState);
+    // };
+  }, []);
 
   if (isLoading) {
     return <Spinner />;
@@ -35,6 +77,16 @@ const StudyRoom = () => {
           </Grid>
         </Box>
       </Container>
+      <Modal
+        isOpen={isOpen}
+        title="스터디룸 생성"
+        closeBtn="취소"
+        actionBtn="나가기"
+        onClickActionBtn={exitStudyRoom}
+        onClose={toggle}
+      >
+        <Typography>스터디 방을 나가시겠습니까?</Typography>
+      </Modal>
     </>
   );
 };
