1e42b4bdf256af314bdef81400521e0413bbf1b7
jungjinbeom <insunok0715@naver.com>
2024-05-02 23:01:07 +0900

    chore: 라우터 경로 원상 복구 - 배포 테스트를 위한 라우터 위치 변경 원상복구

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -28,18 +28,6 @@ export const router = [
     errorElement: <Error />,
     children: [
       {
-        path: ROUTE_PATH.HOME,
-        element: <Home />,
-      },
-      {
-        path: ROUTE_PATH.LOGIN,
-        element: (
-          <AppRoute>
-            <Login />
-          </AppRoute>
-        ),
-      },
-      {
         path: `${ROUTE_PATH.STUDY_ROOMS}/:id`,
         element: (
           <Suspense fallback={<Spinner />}>
@@ -60,6 +48,18 @@ export const router = [
         ),
       },
     ],
+  },
+  {
+    path: ROUTE_PATH.HOME,
+    element: <Home />,
+  },
+  {
+    path: ROUTE_PATH.LOGIN,
+    element: (
+      <AppRoute>
+        <Login />
+      </AppRoute>
+    ),
   },
 ];
 
