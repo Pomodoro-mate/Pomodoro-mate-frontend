3eab426d026a2e17411271316e967b55f150fe04
jungjinbeom <insunok0715@naver.com>
2024-05-21 00:10:12 +0900

    feat: 스터디 진행 컴포넌트 추가 - 스터디 진행 버튼 컴포넌트 추가

---
+++ src/pages/study-room/components/next-step-button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import useSockJSContext from '../hooks/useSockJSContext';
+import useAudio from '../hooks/useAudio';
+import { Button } from '@mui/material';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { Step } from '@/types/study-room.types';
+
+interface SoundClickProps {
+  sound: string;
+  step: Step;
+}
+
+const NextStepButton = ({ sound, step }: SoundClickProps) => {
+  const { goToNextStep } = useSockJSContext();
+
+  const { play } = useAudio({ initialSound: sound });
+
+  return (
+    <Button
+      size="large"
+      type="button"
+      variant="contained"
+      onClick={() => {
+        goToNextStep(step);
+        play();
+      }}
+    >
+      &nbsp; NEXT STEP <KeyboardArrowRightIcon />
+    </Button>
+  );
+};
+
+export default NextStepButton;
