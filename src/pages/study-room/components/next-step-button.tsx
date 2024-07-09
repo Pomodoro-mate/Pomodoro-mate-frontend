cc5e0a1a8a3b5ebd67f4603e829fb6828a292242
jungjinbeom <insunok0715@naver.com>
2024-07-09 20:53:10 +0900

    Chore: 사운드 클릭 함수 생성

--- src/pages/study-room/components/next-step-button.tsx
+++ src/pages/study-room/components/next-step-button.tsx
@@ -4,26 +4,23 @@ import { Button } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { Step } from '@/types/study-room.types';
 
-interface SoundClickProps {
+interface NextStepButtonProps {
   sound: string;
   step: Step;
 }
 
-const NextStepButton = ({ sound, step }: SoundClickProps) => {
+const NextStepButton = ({ sound, step }: NextStepButtonProps) => {
   const { goToNextStep } = useSockJSContext();
 
   const { play } = useAudio({ initialSound: sound });
 
+  const onClick = () => {
+    goToNextStep(step);
+    play();
+  };
+
   return (
-    <Button
-      size="large"
-      type="button"
-      variant="contained"
-      onClick={() => {
-        goToNextStep(step);
-        play();
-      }}
-    >
+    <Button size="large" type="button" variant="contained" onClick={onClick}>
       &nbsp; NEXT STEP <KeyboardArrowRightIcon />
     </Button>
   );
