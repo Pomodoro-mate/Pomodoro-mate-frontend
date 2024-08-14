8d6f5515c4370cf3e2417cb6b6f104d969283286
jungjinbeom <insunok0715@naver.com>
2024-08-14 21:47:15 +0900

    chore: 스타일 수정 작업 - 디자인 미적용 페이지 및 컴포넌트 디자인 적용

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -10,36 +10,35 @@ import useExitRoomModalContext from '@/pages/study-room/hooks/useExitRoomModalCo
 
 const Header = () => {
   const { pathname, state } = useLocation();
-  console.log({ title: state });
+
   const { open } = useExitRoomModalContext();
   const IN_STUDY_ROOM_REGEX = new RegExp(`^${ROUTE_PATH.STUDY_ROOMS}/\\d+$`);
   const isStudyRoomPage = IN_STUDY_ROOM_REGEX.test(pathname);
 
   return (
-    <div className="border-b">
-      <header
-        className={clsx(
-          'h-[4.375rem] w-full flex justify-between shrink-0 items-center my-0 px-40',
-          isStudyRoomPage && 'bg-gray-200',
-        )}
-      >
-        {!isStudyRoomPage ? (
-          <>
-            <img src={logo} />
-            <SwitchController pathname={pathname} />
-          </>
-        ) : (
-          <>
-            <div>{state?.title ?? ''}</div>
-            <div className="flex gap-5">
-              <Setting width={24} height={24} fill="#333333" />
-              <Info width={24} height={24} fill="#333333" />
-              <div onClick={open} className="cursor-pointer">
-                <Exit width={24} height={24} fill="#333333" />
+    <div className="border-b border-achromatic-40">
+      <header className={clsx('h-[4.375rem] my-0 px-40', isStudyRoomPage && 'bg-achromatic-20')}>
+        <nav className="flex justify-between w-full shrink-0 items-center h-full">
+          {!isStudyRoomPage ? (
+            <>
+              <img src={logo} />
+              <SwitchController pathname={pathname} />
+            </>
+          ) : (
+            <>
+              <div>
+                <h4 className="">{state?.title ?? ''}</h4>
               </div>
-            </div>
-          </>
-        )}
+              <div className="flex gap-5">
+                <Setting width={24} height={24} fill="#333333" />
+                <Info width={24} height={24} fill="#333333" />
+                <div onClick={open} className="cursor-pointer">
+                  <Exit width={24} height={24} fill="#333333" />
+                </div>
+              </div>
+            </>
+          )}
+        </nav>
       </header>
     </div>
   );
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,6 @@
 import landingImg from '@/assets/landing.png';
 import Header from '@/components/layout/header/header';
-import { Container, Title } from '@/components/ui';
+import { Container } from '@/components/ui';
 import LoginDialog from '../login/login-dialog';
 
 const Home = () => {
@@ -9,11 +9,9 @@ const Home = () => {
       <Header />
       <Container className="flex flex-row flex-wrap items-center justify-between px-40">
         <div>
-          <Title>
-            <p className="sm:mb-2 font-bold sm:text-3xl">나의</p>
-            <p className="sm:mb-2 font-bold sm:text-3xl">뽀모도로 메이트를 만나</p>
-            <p className="sm:mb-5 font-bold sm:text-3xl">같이 스터디 할까말까?</p>
-          </Title>
+          <h1 className="mb-2">나의</h1>
+          <h1 className="mb-2">뽀모도로 메이트를 만나</h1>
+          <h1 className="mb-5">같이 스터디 할까말까?</h1>
           <LoginDialog btnName="스터디 하러가기" />
         </div>
         <img src={landingImg} alt="landing" width={550} height={550} />
