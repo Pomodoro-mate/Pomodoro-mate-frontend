84db39dcdaa6ef711d0c048b604df4f71f4c6e5d
jungjinbeom <insunok0715@naver.com>
2024-08-20 00:00:34 +0900

    chore: 개행 수정

--- src/pages/study-rooms/study-rooms.tsx
+++ src/pages/study-rooms/study-rooms.tsx
@@ -7,7 +7,6 @@ const StudyRooms = () => {
     <div className="mx-auto">
       <Title className="mb-7">스터디룸 목록</Title>
       <StudyRoomList />
-
       <div className="flex flex-col items-center">
         <p className="mt-4 mb-4">원하는 스터디룸이 없으신가요?</p>
         <CreateStudyRoomDialog />
