e86b0dae9380f1ed5c66117422e38b774ecd959c
jungjinbeom <insunok0715@naver.com>
2024-08-19 23:58:11 +0900

    chore: 중복 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from './components/theme-provider';
 import { routes } from './router';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
     <ThemeProvider defaultTheme="light">
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={routes} />
-      </QueryClientProvider>
+      <RouterProvider router={routes} />
     </ThemeProvider>
   );
 }
