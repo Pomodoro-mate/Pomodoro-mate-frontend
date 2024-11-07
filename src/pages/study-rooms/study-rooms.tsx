2f78df3a5cd88ab4b8721b43f29020825ddb6496
Kimyerim1935 <kimyerim1935@naver.com>
2024-11-07 20:54:38 +0900

    Feat: toast 컴포넌트 사용 예시
    로그인 완료 시 /study-room 에 "로그인이 완료되었습니다" 메시지와 함께 컴포넌트 노출

--- src/pages/study-rooms/study-rooms.tsx
+++ src/pages/study-rooms/study-rooms.tsx
@@ -2,10 +2,13 @@ import { Title } from '@/components/ui/title';
 import CreateStudyRoomDialog from './components/create-study-room-dialog';
 import StudyRoomList from './components/study-room-list';
 
+import Toast from '@/components/common/toast/toast';
+
 const StudyRooms = () => {
   return (
     <div className="mx-auto">
       <Title className="mb-7">스터디룸 목록</Title>
+      <Toast type="success" message="로그인이 완료되었습니다." autoClose />
       <StudyRoomList />
       <div className="flex flex-col items-center">
         <p className="mt-4 mb-4">원하는 스터디룸이 없으신가요?</p>
