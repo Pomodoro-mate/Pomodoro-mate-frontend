bd5b2d200f68a479e8dcf7ae70cd049751c445b1
Kimyerim1935 <kimyerim1935@naver.com>
2024-11-11 20:23:23 +0900

    Chore: toast 노출 커스텀 훅 분리 & 코드리뷰 후 수정사항 반영

--- src/components/common/toast/toast.tsx
+++ src/components/common/toast/toast.tsx
@@ -1,52 +1,31 @@
+import useToast from '@/hooks/useToast';
 import clsx from 'clsx';
-import { useEffect, useState } from 'react';
 
 type ToastProps = {
-  type: 'success' | 'error' | 'warning' | 'info';
+  type?: 'success' | 'error' | 'warning' | 'info';
   message: string;
   autoClose?: boolean;
+  duration?: number;
 };
 
-const Toast: React.FC<ToastProps> = ({ type = 'info', message, autoClose = false }) => {
-  const [visible, setVisible] = useState(true);
+const toastBgColor = {
+  success: 'bg-green-100 text-green-800',
+  error: 'bg-red-100 text-red-800',
+  warning: 'bg-yellow-100 text-yellow-800',
+  info: 'bg-blue-100 text-blue-800',
+};
 
-  useEffect(() => {
-    if (visible && autoClose) {
-      const timer = setTimeout(() => {
-        setVisible(false);
-      }, 3000);
-
-      return () => clearTimeout(timer);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [visible]);
-
-  const handleClose = () => {
-    setVisible(false);
-  };
+const Toast: React.FC<ToastProps> = ({ type = 'info', message, autoClose = false, duration }) => {
+  const { visible, handleClose } = useToast({ type, message, autoClose, duration });
 
   if (!visible) return null;
-
-  const getAlertStyle = () => {
-    switch (type) {
-      case 'success':
-        return 'bg-green-100 text-green-800';
-      case 'error':
-        return 'bg-red-100 text-red-800';
-      case 'warning':
-        return 'bg-yellow-100 text-yellow-800';
-      case 'info':
-      default:
-        return 'bg-blue-100 text-blue-800';
-    }
-  };
 
   return (
     <div
       className={clsx(
         'fixed p-4 rounded-md top-[5.375rem]',
-        'w-[400px] right-[100px] z-[9999] ',
-        getAlertStyle(),
+        'w-[400px] right-[100px] z-[9999]',
+        toastBgColor[type],
       )}
     >
       <div className="flex justify-between items-center">
